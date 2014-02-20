0104d5f3d09a34816daab0245b9d14f1165974d0
Sergey Panfilov <sergray@gmail.com>
2014-02-20 18:32:28 +0400

    version 0.0.2: extend interfaces of Query and URL

--- easyurl.py
+++ easyurl.py
@@ -10,16 +10,17 @@ class Query(object):
     Interpolates query with urllib.urlencode.
     """
 
-    __slots__ = ('params', )
-
     def __init__(self, query=''):
-        self.params = urlparse.parse_qs(query)
+        self._params = urlparse.parse_qs(query)
+
+    def __repr__(self):
+        return repr(self._params)
         
     def __str__(self):
-        return urllib.urlencode(self.params, doseq=True)
+        return urllib.urlencode(self._params, doseq=True)
     
     def __getitem__(self, item):
-        val = self.params[item]
+        val = self._params[item]
         if isinstance(val, list) and len(val) == 1:
             return val[0]
         else:
@@ -28,11 +29,21 @@ class Query(object):
     def __setitem__(self, item, val):
         if not isinstance(val, list):
             val = [val]
-        self.params[item] = val
+        self._params[item] = val
+
+    def __iter__(self):
+        for k in self._params:
+            yield self._params[k]
+
+    def __len__(self):
+        return len(self._params)
+
+    def keys(self):
+        return self._params.keys()
         
     def update(self, mapping):
         # TODO validation of values in mapping, they must not be nested mappings
-        self.params.update(mapping)
+        self._params.update(mapping)
 
 
 class URL(object):
@@ -48,18 +59,42 @@ class URL(object):
     query attribute is Query instance.
     """
     
-    __slots__ = urlparse.ParseResult._fields
+    _fields = urlparse.ParseResult._fields
 
     def __init__(self, url):
-        self.scheme, self.netloc, self.path, self.params, query, self.fragment = urlparse.urlparse(url)
-        self.query = Query(query)
+        for field, value in zip(self._fields, urlparse.urlparse(url)):
+            setattr(self, field, value)
+
+    @property
+    def query(self):
+        return self._query
+
+    @query.setter
+    def query(self, value):
+        if isinstance(value, Query):
+            self._query = value
+        else:
+            self._query = Query(value)
     
     def __str__(self):
         return urlparse.urlunparse(str(s) for s in self)
-    
+
+    def __getitem__(self, field_idx):
+        if isinstance(field_idx, slice):
+            return [getattr(self, field) for field in self._fields[field_idx]]
+        else:
+            return getattr(self, self._fields[field_idx])
+
+    def __setitem__(self, field_idx, val):
+        if isinstance(field_idx, slice):
+            for field in self._fields[field_idx]:
+                setattr(self, field, val)
+        else:
+            setattr(self, self._fields[field_idx], val)
+
     def __iter__(self):
-        for field in urlparse.ParseResult._fields:
+        for field in self._fields:
             yield getattr(self, field)
             
     def __len__(self):
-        return len(urlparse.ParseResult._fields)
+        return len(self._fields)
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='easyurl',
-    version='0.0.1',
+    version='0.0.2',
     description='Easy handling of URLs',
     long_description=readme,
     author='Sergey Panfilov',
